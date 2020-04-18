7d15ce6af370d99311f20a33932bdb503fec652e
Павел Осипов <pavelosipov@iMac-Pavel.Dlink>
2020-04-18 15:13:49 +0300

    Личный проект: покоритель файлов

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -8,60 +8,16 @@ const {getRandomInt, shuffle} = require(`../../utils`);
 const {ExitCode} = require(`../../constants`);
 
 const DEFAULT_COUNT = 1;
+const MAX_MONTH_RANGE = 3;
 const FILE_NAME = `mocks.json`;
+const FILE_TITLES_PATH = `./data/titles.txt`;
+const FILE_DESCRIPTIONS_PATH = `./data/descriptions.txt`;
+const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 const MaxCount = {
   blog: 1000,
   category: 9,
   announce: 5
 };
-const MAX_MONTH_RANGE = 3;
-const TITLES = [
-  `Ёлки. История деревьев`,
-  `Как перестать беспокоиться и начать жить`,
-  `Как достигнуть успеха не вставая с кресла`,
-  `Обзор новейшего смартфона`,
-  `Лучше рок-музыканты 20-века`,
-  `Как начать программировать`,
-  `Учим HTML и CSS`,
-  `Что такое золотое сечение`,
-  `Как собрать камни бесконечности`,
-  `Борьба с прокрастинацией`,
-  `Рок — это протест`,
-  `Самый лучший музыкальный альбом этого года`
-];
-const DESCRIPTIONS = [
-  `Ёлки — это не просто красивое дерево. Это прочная древесина.`,
-  `Первая большая ёлка была установлена только в 1938 году.`,
-  `Вы можете достичь всего. Стоит только немного постараться и запастись книгами.`,
-  `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
-  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
-  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
-  `Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
-  `Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.`,
-  `Программировать не настолько сложно, как об этом говорят.`,
-  `Простые ежедневные упражнения помогут достичь успеха.`,
-  `Это один из лучших рок-музыкантов.`,
-  `Он написал больше 30 хитов.`,
-  `Из под его пера вышло 8 платиновых альбомов.`,
-  `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.`,
-  `Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?`,
-  `Достичь успеха помогут ежедневные повторения.`,
-  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
-  `Как начать действовать? Для начала просто соберитесь.`,
-  `Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравится только игры.`,
-  `Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`
-];
-const CATEGORIES = [
-  `Деревья`,
-  `За жизнь`,
-  `Без рамки`,
-  `Разное`,
-  `IT`,
-  `Музыка`,
-  `Кино`,
-  `Программирование`,
-  `Железо`,
-];
 
 const calculateRandomDate = (dateRange) => {
   const now = new Date();
@@ -70,13 +26,25 @@ const calculateRandomDate = (dateRange) => {
   return dateFormat(randomMonth, `yyyy-mm-dd HH:MM:ss`);
 };
 
-const generateBlogs = (count) => (
+const readContent = async (filePath) => {
+  try {
+    return (await fs.readFile(filePath, `utf8`)).trim().split(`\n`);
+  } catch (err) {
+    console.log(chalk.red(err));
+
+    return [];
+  }
+};
+
+const getContent = async (...cb) => await Promise.all([...cb]);
+
+const generateBlogs = (count, titles, descriptions, categories) => (
   Array(count).fill({}).map(() => ({
-    title: shuffle(TITLES)[getRandomInt(0, MaxCount.category)],
+    title: shuffle(titles)[getRandomInt(0, MaxCount.category)],
     createdDate: calculateRandomDate(getRandomInt(0, MAX_MONTH_RANGE)),
-    announce: shuffle(DESCRIPTIONS).slice(1, MaxCount.announce).join(` `),
-    fullText: shuffle(DESCRIPTIONS).slice(1, DESCRIPTIONS.length).join(` `),
-    category: shuffle(CATEGORIES).slice(0, getRandomInt(1, MaxCount.category))
+    announce: shuffle(descriptions).slice(1, MaxCount.announce).join(` `),
+    fullText: shuffle(descriptions).slice(1, descriptions.length).join(` `),
+    category: shuffle(categories).slice(0, getRandomInt(1, MaxCount.category))
   }))
 );
 
@@ -90,7 +58,12 @@ module.exports = {
       process.exit(ExitCode.error);
     }
 
-    const content = JSON.stringify(generateBlogs(userCount), null, ` `);
+    const content = await getContent(
+      readContent(FILE_TITLES_PATH),
+      readContent(FILE_DESCRIPTIONS_PATH),
+      readContent(FILE_CATEGORIES_PATH)
+    ).then((contents) => JSON
+      .stringify(generateBlogs(userCount, contents[0], contents[1], contents[2]), null, ` `));
 
     try {
       await fs.writeFile(FILE_NAME, content);
