a83bb3e93f6bda5bb2c6d2c6991645b9e1aacf5d
Павел Осипов <pavelosipov@iMac-Pavel.Dlink>
2020-04-20 10:22:18 +0300

    Внес правки по замечаниям

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -7,17 +7,17 @@ const fs = require(`fs`).promises;
 const {getRandomInt, shuffle} = require(`../../utils`);
 const {ExitCode} = require(`../../constants`);
 
-const DEFAULT_COUNT = 1;
-const MAX_MONTH_RANGE = 3;
 const FILE_NAME = `mocks.json`;
 const FILE_TITLES_PATH = `./data/titles.txt`;
 const FILE_DESCRIPTIONS_PATH = `./data/descriptions.txt`;
 const FILE_CATEGORIES_PATH = `./data/categories.txt`;
-const MaxCount = {
-  blog: 1000,
-  category: 9,
-  announce: 5
-};
+const DEFAULT_COUNT = 1;
+const MAX_MONTH_RANGE = 3;
+const MAX_COUNT = Object.freeze({
+  BLOG: 1000,
+  CATEGORY: 9,
+  ANNOUNCE: 5
+});
 
 const calculateRandomDate = (dateRange) => {
   const now = new Date();
@@ -28,23 +28,27 @@ const calculateRandomDate = (dateRange) => {
 
 const readContent = async (filePath) => {
   try {
-    return (await fs.readFile(filePath, `utf8`)).trim().split(`\n`);
-  } catch (err) {
-    console.log(chalk.red(err));
+    const content = await fs.readFile(filePath, `utf8`);
 
-    return [];
+    if (!content.trim().length) {
+      throw new Error(`Файл пуст`);
+    }
+
+    return content.trim().split(`\n`);
+  } catch (err) {
+    throw err;
   }
 };
 
-const getContent = async (...cb) => Promise.all([...cb]);
+const runParallel = async (...cb) => Promise.all([...cb]);
 
 const generateBlogs = (count, titles, descriptions, categories) => (
   Array(count).fill({}).map(() => ({
-    title: shuffle(titles)[getRandomInt(0, MaxCount.category)],
+    title: shuffle(titles)[getRandomInt(0, MAX_COUNT.CATEGORY)],
     createdDate: calculateRandomDate(getRandomInt(0, MAX_MONTH_RANGE)),
-    announce: shuffle(descriptions).slice(1, MaxCount.announce).join(` `),
+    announce: shuffle(descriptions).slice(1, MAX_COUNT.ANNOUNCE).join(` `),
     fullText: shuffle(descriptions).slice(1, descriptions.length).join(` `),
-    category: shuffle(categories).slice(0, getRandomInt(1, MaxCount.category))
+    category: shuffle(categories).slice(0, getRandomInt(1, MAX_COUNT.CATEGORY))
   }))
 );
 
@@ -53,24 +57,27 @@ module.exports = {
   async run(count) {
     const userCount = parseInt(count, 10) || DEFAULT_COUNT;
 
-    if (userCount > MaxCount.blog) {
-      console.log(chalk.red(`Не больше ${MaxCount.blog} публикаций`));
+    if (userCount > MAX_COUNT.BLOG) {
+      console.log(chalk.red(`Не больше ${MAX_COUNT.BLOG} публикаций`));
       process.exit(ExitCode.error);
     }
 
-    const content = await getContent(
-      readContent(FILE_TITLES_PATH),
-      readContent(FILE_DESCRIPTIONS_PATH),
-      readContent(FILE_CATEGORIES_PATH)
-    ).then((contents) => JSON
-      .stringify(generateBlogs(userCount, contents[0], contents[1], contents[2]), null, ` `));
-
     try {
+      const content = await runParallel(
+          readContent(FILE_TITLES_PATH),
+          readContent(FILE_DESCRIPTIONS_PATH),
+          readContent(FILE_CATEGORIES_PATH)
+      ).then(([titles, descriptions, categories]) => JSON
+        .stringify(generateBlogs(userCount, titles, descriptions, categories), null, ` `));
+
       await fs.writeFile(FILE_NAME, content);
       console.log(chalk.green(`Operation success. File created.`));
+
       process.exit(ExitCode.success);
     } catch (err) {
-      console.log(chalk.red(`Can't write data to file...`));
+      console.error(chalk.red(`Mock file generation failed`));
+      console.error(chalk.red(err));
+
       process.exit(ExitCode.error);
     }
   }
