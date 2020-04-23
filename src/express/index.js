2b63a87e9311bea164df8e74c51bf6951104d706
Павел Осипов <pavelosipov@iMac-Pavel.Dlink>
2020-04-23 22:37:05 +0300

    Личный проект: шаблонизируй это

--- src/express/index.js
+++ src/express/index.js
@@ -1,26 +1,29 @@
 'use strict';
 
+const path = require(`path`);
 const express = require(`express`);
 const chalk = require(`chalk`);
 
 const app = express();
-const indexRoutes = require(`./routes/index-routes`);
-const registerRoutes = require(`./routes/register-routes`);
-const loginRoutes = require(`./routes/login-routes`);
-const myRoutes = require(`./routes/my-routes`);
-const articlesRoutes = require(`./routes/articles-routes`);
-const searchRoutes = require(`./routes/search-routes`);
-const categoriesRoutes = require(`./routes/categories-routes`);
+const mainRoutes = require(`./routes/main-routes`);
+const adminRoutes = require(`./routes/admin-routes`);
 
 const DEFAULT_PORT = 8080;
+const PUBLIC_DIR = `public`;
 
-app.use(`/`, indexRoutes);
-app.use(`/register`, registerRoutes);
-app.use(`/login`, loginRoutes);
-app.use(`/my`, myRoutes);
-app.use(`/articles`, articlesRoutes);
-app.use(`/search`, searchRoutes);
-app.use(`/categories`, categoriesRoutes);
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.use(`/`, mainRoutes);
+app.use(`/`, adminRoutes);
+app.use((req, res) => {
+  res.status(404).render(`errors/404`);
+});
+app.use((err, req, res, next) => {
+  res.status(500).render(`errors/500`);
+  next();
+});
 
 app.listen(DEFAULT_PORT, (err) => {
   if (err) {
